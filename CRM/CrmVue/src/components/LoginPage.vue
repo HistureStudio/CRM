f08f08fb1f8c51b2d51539c3b93736efa3450ee3
周品均 <ja607222@gmail.com>
2025-02-15 16:38:06 +0800

    feat: add vue project

---
+++ CRM/CrmVue/src/components/LoginPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <TextBar />
+</template>
+
+<script>
+import TextBar from './LoginTextBar.vue'
+
+export default {
+  components: { TextBar },
+}
+</script>
---
+++ CRM/CrmVue/src/components/NewRegisterTextBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-text-bar">
+    <slot>新用戶註冊</slot>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.login-text-bar {
+  width: 100%;
+  background-color: #ffd99b;
+  color: #000000;
+  border: none;
+  border-radius: 8px;
+  padding: 12px 16px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
